a488360f762f4d96af6fa226d8acce620c31f6ac
WilllllWang <151107396+WilllllWang@users.noreply.github.com>
2024-09-21 21:53:49 +0800

    ignore template

--- template.cpp
+++ template.cpp
@@ -1,52 +1,35 @@
 #include <bits/stdc++.h>
-    
+ 
+ 
 using namespace std;
 using ll = long long;
+const int mod = 1e9 + 7;
+const int ub = 1e9 + 1;
+ 
+void solve() { 
 
-
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (auto& x: a)
-        cin >> x;
-    vector<int> b(n);
-    for (auto& x: b)
-        cin >> x;
-    int m;
-    cin >> m;
-    unordered_map<int, int> count;
-    for (int i = 0; i < m; i++) {
-        int x;
-        cin >> x;
-        count[x]++;
-    }
     
-    for (int i = 0; i < n; i++) {
-        if (a[i] != b[i]) {
-            count[b[i]]--;
-            if (count[b[i]] < 0) {
-                cout << "NO" << endl;
-                return;
-            }
-        }
-    }
-    cout << "YES" << endl;
-
 }
 
-
-
-
+ 
+void testTime() {
+    auto start = chrono::steady_clock::now();
+    auto end = chrono::steady_clock::now();
+    cout << "Total time = " << chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "(micro)" << endl;
+}
+ 
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
+ 
     int t = 1;
-    cin >> t;
+    //cin >> t;
     while (t--) {
+        //testTime();
         solve();
     }
+ 
     return 0;
 }
-
-
+ 
